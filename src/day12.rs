32726c0171f787d688b603a98b258c0dcceee7b3
Christian Briones <cwbriones@gmail.com>
2021-12-12 14:47:57 -0800

    finally get day 12 working without deduping

--- src/day12.rs
+++ src/day12.rs
@@ -1,131 +1,84 @@
 use crate::prelude::*;
 
-#[derive(Debug, Clone, PartialEq, Hash, Eq)]
-enum Node {
-    Small(String),
-    Big(String),
-}
-
-impl std::str::FromStr for Node {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let node = match s {
-            s if s.chars().all(|c| c.is_lowercase()) => Node::Small(s.into()),
-            s => Node::Big(s.into()),
-        };
-        Ok(node)
-    }
-}
+const MAX_NODES: usize = 32;
 
 #[derive(Debug, Clone)]
 struct Graph {
-    edges: HashMap<Node, Vec<Node>>,
+    start: usize,
+    end: usize,
+    small: Vec<bool>,
+    edges: HashMap<usize, Vec<usize>>,
 }
 
 fn parse(input: &str) -> Graph {
-    let mut edges = HashMap::default();
-    for line in input.lines() {
+    let iter = input.lines().map(|line| {
         let mut split = line.split('-');
-        let from = split.next().unwrap().parse::<Node>().unwrap();
-        let to = split.next().unwrap().parse::<Node>().unwrap();
-        edges
-            .entry(from.clone())
-            .or_insert_with(Vec::new)
-            .push(to.clone());
-        edges.entry(to).or_insert_with(Vec::new).push(from);
+        let from = split.next().unwrap();
+        let to = split.next().unwrap();
+        (from, to)
+    });
+    let mut ids = HashMap::default();
+    let mut small = Vec::new();
+    let mut create_node = |k: &str| {
+        *ids.entry(k.to_owned()).or_insert_with(|| {
+            small.push(k.chars().all(|c| c.is_lowercase()));
+            small.len() - 1
+        })
+    };
+    let start = create_node("start");
+    let end = create_node("end");
+    let mut edges = HashMap::default();
+    for (k, v) in iter {
+        let kid = create_node(k);
+        let vid = create_node(v);
+        edges.entry(kid).or_insert_with(Vec::new).push(vid);
+        if kid != start && vid != end {
+            edges.entry(vid).or_insert_with(Vec::new).push(kid);
+        }
     }
-    Graph { edges }
+    if small.len() > MAX_NODES {
+        panic!("too many nodes in input: {} > {}", small.len(), MAX_NODES);
+    }
+    Graph {
+        start,
+        end,
+        small,
+        edges,
+    }
 }
 
 fn part_one(graph: Graph) -> usize {
-    // Try all paths with backtracking
-    // let mut seen = HashSet::default();
-    let mut path = String::new();
-    let mut paths = HashSet::default();
-    search_one(&graph, &mut path, &mut paths);
-    paths.len()
+    println!("{:?}", graph);
+    let visited = [false; MAX_NODES];
+    search(&graph, graph.start, visited, false)
 }
 
 fn part_two(graph: Graph) -> usize {
-    let start = Node::Small("start".into());
-    let small_iter = graph.edges.keys().filter_map(|n| {
-        if let Node::Small(s) = n {
-            Some(s)
-        } else {
-            None
-        }
-    });
-
-    let mut remaining = small_iter
-        .clone()
-        .cloned()
-        .map(|k| (k, 1))
-        .collect::<HashMap<String, usize>>();
-    let mut path = String::new();
-    let mut paths = HashSet::default();
-    for s in small_iter.filter(|s| *s != "start") {
-        // mark each small node besides start as "can visit twice"
-        *remaining.get_mut(s).unwrap() += 1;
-        search(&graph, &start, &mut remaining, &mut path, &mut paths);
-        path.clear();
-        *remaining.get_mut(s).unwrap() -= 1;
-    }
-    paths.len()
-}
-
-fn search_one(graph: &Graph, path: &mut String, visited: &mut HashSet<String>) {
-    let start = Node::Small("start".into());
-    let small_iter = graph.edges.keys().filter_map(|n| {
-        if let Node::Small(s) = n {
-            Some(s)
-        } else {
-            None
-        }
-    });
-    let mut remaining = small_iter
-        .clone()
-        .cloned()
-        .map(|k| (k, 1))
-        .collect::<HashMap<String, usize>>();
-    search(graph, &start, &mut remaining, path, visited);
+    let visited = [false; MAX_NODES];
+    search(&graph, graph.start, visited, true)
 }
 
 fn search(
     graph: &Graph,
-    node: &Node,
-    remaining: &mut HashMap<String, usize>,
-    path: &mut String,
-    visited: &mut HashSet<String>,
-) {
-    let s = match node {
-        Node::Small(s) => s,
-        Node::Big(s) => s,
-    };
-    let len = s.len();
-    path.push(' ');
-    path.push_str(s);
-    match node {
-        Node::Small(s) if s == "end" => {
-            visited.insert(path.clone());
-        }
-        Node::Small(s) if remaining.get(s).map(|v| *v > 0).unwrap() => {
-            *remaining.get_mut(s).unwrap() -= 1;
-            let edges = graph.edges.get(node);
-            for v2 in edges.iter().flat_map(|v| v.iter()) {
-                search(graph, v2, remaining, path, visited);
-            }
-            *remaining.get_mut(s).unwrap() += 1;
-        }
-        Node::Big(_) => {
-            let edges = graph.edges.get(node);
-            for v2 in edges.iter().flat_map(|v| v.iter()) {
-                search(graph, v2, remaining, path, visited);
-            }
-        }
-        _ => {}
+    node: usize,
+    mut visited: [bool; MAX_NODES],
+    can_visit_twice: bool,
+) -> usize {
+    if node == graph.end {
+        return 1;
     }
-    path.truncate(path.len() - len - 1);
+    visited[node] = graph.small[node];
+    let edges = graph.edges.get(&node);
+    let neighbors = edges
+        .iter()
+        .flat_map(|v| v.iter().cloned())
+        .filter(|&n| !visited[n] || can_visit_twice)
+        .collect::<Vec<_>>();
+    let mut total = 0;
+    for n in neighbors {
+        total += search(graph, n, visited, !visited[n] && can_visit_twice);
+    }
+    total
 }
 
 pub fn run(runner: &Runner) {
